99558a31a52d16323bf305c907fff9c6be804cb8
Dean Tognolini <deantognolini@gmail.com>
2020-07-02 23:52:46 +1000

    new file

--- Python/DevNet/RESTCONF/GET_interfaces_native.py
+++ Python/DevNet/RESTCONF/GET_interfaces_native.py
@@ -1,16 +1,19 @@
-import requests
+import requests, json
 
+# Define YANG Modules and Container (this is the DevNet always on IOS-XE Router)
 url = "https://ios-xe-mgmt-latest.cisco.com:9443/restconf/data/Cisco-IOS-XE-interfaces-oper:interfaces"
 
-user = 'root'
-pw = 'D_Vay!_10&'
+# Credentials
+user = "root"
+pw = "D_Vay!_10&"
 
 payload = {}
 headers = {
-  'Accept': 'application/yang-data+json',
-  'Content-Type': 'application/yang-data+json'
+    "Accept": "application/yang-data+json",
+    "Content-Type": "application/yang-data+json",
 }
 
-response = requests.request("GET", url, headers=headers, data = payload)
+response = requests.request(
+    "GET", url, auth=(user, pw), headers=headers, data=payload, verify=False).json()
 
-print(response.text.encode('utf8'))
+print(json.dumps(response, indent=2))
